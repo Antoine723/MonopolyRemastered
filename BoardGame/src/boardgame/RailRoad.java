c75bd1c21fe72e9d2e6124b411689e5250e38c17
Blasselle <thib.blasselle@gmail.com>
2020-11-07 11:34:43 +0100

    ajout d'une fonction Compute dans RailRoad
    à checker

--- BoardGame/src/boardgame/RailRoad.java
+++ BoardGame/src/boardgame/RailRoad.java
@@ -32,4 +32,40 @@ public class RailRoad extends Property {
     public int computeRent(){
         return super.getRent()*number;
     }
+    
+    
+    public int computing(Property prop)
+    {
+        if(prop instanceof Avenue)
+        {
+            return prop.getRent();
+        }
+        else if (prop instanceof RailRoad)
+        {
+            switch(prop.getAssociatedPlayer().getNumberOfRailRoads())
+            {
+                case 2:
+                    prop.setRent(prop.getRent() + (int) 0.5*prop.getRent());
+                    break;
+
+                case 3:
+                    prop.setRent(prop.getRent() + (int) 1.5*prop.getRent());
+                    break;
+
+                case 4:
+                    prop.setRent(prop.getRent() + (int) 2*prop.getRent());
+                    break;
+            }
+            return prop.getRent();
+        }
+        
+        else
+        {
+            if (prop.getAssociatedPlayer().getNumberOfCompanies() == 2)
+            {
+                prop.setRent(prop.getRent() + (int) 2*prop.getRent());
+            }
+            return prop.getRent();
+        }
+    }
 }
